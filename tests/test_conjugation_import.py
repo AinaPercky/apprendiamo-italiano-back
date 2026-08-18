50bd6784aa382fc89029e30547efc518152c516a
AinaPercky <8.4370954e+07+AinaPercky@users.noreply.github.com>
2026-08-18 02:58:45 +0000

    feat: add reflexive Italian conjugations

--- tests/test_conjugation_import.py
+++ tests/test_conjugation_import.py
@@ -19,3 +19,29 @@ class ConjugationCorpusTests(unittest.TestCase):
         self.assertEqual(forms[0]["form_text"], "sono")
         self.assertEqual(forms[1]["person_label"], "tu")
         self.assertEqual(forms[1]["form_text"], "sei")
+
+    def test_reflexive_verbs_are_present_and_have_normalized_forms(self):
+        verbs, _, _ = parse_source_dataset(CORPUS_PATH)
+        by_infinitive = {verb["infinitive"]: verb for verb in verbs}
+        expected = {
+            "alzarsi", "lavarsi", "vestirsi", "svegliarsi", "chiamarsi",
+            "divertirsi", "sentirsi", "sedersi", "addormentarsi", "pentirsi",
+            "accorgersi", "iscriversi", "servirsi", "sbrigarsi", "trasferirsi",
+        }
+        self.assertTrue(expected.issubset(by_infinitive))
+        self.assertGreaterEqual(sum(infinitive.endswith("si") for infinitive in by_infinitive), 35)
+
+        alzarsi = by_infinitive["alzarsi"]
+        presente = alzarsi["blocks"][("Indicativo", "Presente")]["forms"]
+        self.assertEqual([form["form_text"] for form in presente[:3]], ["mi alzo", "ti alzi", "si alza"])
+        self.assertEqual(alzarsi["blocks"][("Infinito", "Presente")]["forms"][0]["form_text"], "alzarsi")
+
+        pentirsi = by_infinitive["pentirsi"]
+        pentirsi_presente = pentirsi["blocks"][("Indicativo", "Presente")]["forms"]
+        self.assertEqual(pentirsi_presente[0]["form_text"], "mi pento")
+        self.assertNotIn("mi mi", " ".join(form["form_text"] for form in pentirsi_presente))
+
+        accorgersi = by_infinitive["accorgersi"]
+        accorgersi_passato = accorgersi["blocks"][("Indicativo", "Passato prossimo")]["forms"]
+        self.assertEqual(accorgersi_passato[0]["form_text"], "mi sono accorto")
+        self.assertEqual(accorgersi_passato[3]["form_text"], "ci siamo accorti")
